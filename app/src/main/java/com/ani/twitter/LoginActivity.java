285eb2c4c8d601c5a435f3b9699a0c816a9c0846
Anirudh Padmarao <asp76@cornell.edu>
2016-10-30 17:05:23 -0700

    Add style to login screen

--- app/src/main/java/com/ani/twitter/LoginActivity.java
+++ app/src/main/java/com/ani/twitter/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Toast;
 
 import com.codepath.oauth.OAuthLoginActionBarActivity;
 
