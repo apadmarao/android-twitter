1775cad89c1ec591079d32aebd8650836eea26d7
Anirudh Padmarao <asp76@cornell.edu>
2016-11-06 20:08:45 -0800

    Extract profile header into fragment

--- app/src/main/java/com/ani/twitter/activities/ProfileActivity.java
+++ app/src/main/java/com/ani/twitter/activities/ProfileActivity.java
@@ -1,34 +1,18 @@
 package com.ani.twitter.activities;
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Html;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.ani.twitter.R;
-import com.ani.twitter.TwitterApplication;
+import com.ani.twitter.fragments.ProfileHeaderFragment;
 import com.ani.twitter.fragments.UserTimelineFragment;
 import com.ani.twitter.models.User;
-import com.ani.twitter.network.TwitterClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
-import com.squareup.picasso.Picasso;
-
-import org.json.JSONObject;
-
-import java.text.NumberFormat;
-
-import cz.msebera.android.httpclient.Header;
 
 public class ProfileActivity extends AppCompatActivity {
 
     public static final String USER_EXTRA = "user";
-
-    private TwitterClient client;
-    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,64 +22,16 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        user = (User) getIntent().getSerializableExtra(USER_EXTRA);
-        if (user == null) {
-            client = TwitterApplication.getRestClient();
-            client.getUserInfo(new JsonHttpResponseHandler() {
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                    user = User.fromJSON(response);
-                    getSupportActionBar().setTitle("@" + user.getName());
-                    populateProfileHeader(user);
-                }
+        User user = (User) getIntent().getSerializableExtra(USER_EXTRA);
+        String screenName = user == null ? null : user.getScreenName();
 
-                // FIXME : handle error
-            });
-        } else {
-            getSupportActionBar().setTitle("@" + user.getName());
-            populateProfileHeader(user);
-        }
-
-        String screenName = user != null ? user.getScreenName() : null;
         if (savedInstanceState == null) {
             UserTimelineFragment fragmentUserTimeline = UserTimelineFragment.newInstance(screenName);
-
+            ProfileHeaderFragment fragmentHeader = ProfileHeaderFragment.newInstance(user);
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.flProfileHeader, fragmentHeader);
             ft.replace(R.id.flContainer, fragmentUserTimeline);
             ft.commit();
         }
-    }
-
-    private void populateProfileHeader(User user) {
-        ImageView ivProfileBannerImage = (ImageView) findViewById(R.id.ivProfileBannerImage);
-        ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
-        TextView tvProfileName = (TextView) findViewById(R.id.tvProfileName);
-        TextView tvProfileScreenName = (TextView) findViewById(R.id.tvProfileScreenName);
-        TextView tvProfileTagline = (TextView) findViewById(R.id.tvProfileTagline);
-        TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
-        TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
-
-        if (user.getProfileBannerUrl() == null || user.getProfileBannerUrl().isEmpty()) {
-            ivProfileImage.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        } else {
-            Picasso.with(this).load(user.getProfileBannerUrl()).into(ivProfileBannerImage);
-        }
-        Picasso.with(this).load(user.getProfileImageUrl()).into(ivProfileImage);
-
-        Resources res = getResources();
-
-        tvProfileName.setText(user.getName());
-        tvProfileScreenName.setText(String.format(res.getString(R.string.handle), user.getScreenName()));
-        tvProfileTagline.setText(user.getTagline());
-
-        tvFollowing.setText(Html.fromHtml(String.format(res.getString(R.string.following),
-                formatCount(user.getFollowingCount()))));
-        tvFollowers.setText(Html.fromHtml(String.format(res.getString(R.string.followers),
-                formatCount(user.getFollowersCount()))));
-    }
-
-    private String formatCount(int count) {
-        return NumberFormat.getNumberInstance(getResources().getConfiguration().locale)
-                .format(count);
     }
 }
---
+++ app/src/main/java/com/ani/twitter/fragments/ProfileHeaderFragment.java
@@ -0,0 +1,131 @@
+package com.ani.twitter.fragments;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ani.twitter.R;
+import com.ani.twitter.TwitterApplication;
+import com.ani.twitter.models.User;
+import com.ani.twitter.network.TwitterClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONObject;
+
+import java.text.NumberFormat;
+
+import cz.msebera.android.httpclient.Header;
+
+public class ProfileHeaderFragment extends Fragment {
+
+    private static final String USER_EXTRA = "user";
+
+    private TwitterClient client;
+    private User user;
+
+    private ImageView ivProfileBannerImage;
+    private ImageView ivProfileImage;
+    private TextView tvProfileName;
+    private TextView tvProfileScreenName;
+    private TextView tvProfileTagline;
+    private TextView tvFollowing;
+    private TextView tvFollowers;
+
+    public static ProfileHeaderFragment newInstance(User user) {
+        ProfileHeaderFragment fragment = new ProfileHeaderFragment();
+        Bundle args = new Bundle();
+        args.putSerializable(USER_EXTRA, user);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        client = TwitterApplication.getRestClient();
+
+        user = (User) getArguments().getSerializable(USER_EXTRA);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile_header, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ivProfileBannerImage = (ImageView) view.findViewById(R.id.ivProfileBannerImage);
+        ivProfileImage = (ImageView) view.findViewById(R.id.ivProfileImage);
+        tvProfileName = (TextView) view.findViewById(R.id.tvProfileName);
+        tvProfileScreenName = (TextView) view.findViewById(R.id.tvProfileScreenName);
+        tvProfileTagline = (TextView) view.findViewById(R.id.tvProfileTagline);
+        tvFollowing = (TextView) view.findViewById(R.id.tvFollowing);
+        tvFollowers = (TextView) view.findViewById(R.id.tvFollowers);
+
+        if (user == null) {
+            // loading self profile from toolbar
+            populateHeader();
+        } else {
+            // clicked on user profile image
+            populateHeaderWithUser(user);
+        }
+    }
+
+    private void populateHeader() {
+        // FIXME : Handle error
+
+        client.getUserInfo(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                user = User.fromJSON(response);
+                // getActivity().getSupportActionBar().setTitle("@" + user.getName());
+                populateHeaderWithUser(user);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable,
+                    JSONObject errorResponse) {
+                // FIXME : Handle error
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+
+    }
+
+    private void populateHeaderWithUser(User user) {
+        if (user.getProfileBannerUrl() == null || user.getProfileBannerUrl().isEmpty()) {
+            ivProfileImage.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        } else {
+            Picasso.with(getActivity()).load(user.getProfileBannerUrl()).into(ivProfileBannerImage);
+        }
+        Picasso.with(getActivity()).load(user.getProfileImageUrl()).into(ivProfileImage);
+
+        Resources res = getResources();
+        tvProfileName.setText(user.getName());
+        tvProfileScreenName.setText(String.format(res.getString(R.string.handle), user.getScreenName()));
+        tvProfileTagline.setText(user.getTagline());
+
+        tvFollowing.setText(Html.fromHtml(String.format(res.getString(R.string.following),
+                formatCount(user.getFollowingCount()))));
+        tvFollowers.setText(Html.fromHtml(String.format(res.getString(R.string.followers),
+                formatCount(user.getFollowersCount()))));
+    }
+
+    private String formatCount(int count) {
+        return NumberFormat.getNumberInstance(getResources().getConfiguration().locale)
+                .format(count);
+    }
+}
